37b7f4dba9b36324f4a7e105a383d70aadeb8026
Mark Burns <markthedeveloper@googlemail.com>
2010-07-13 10:33:29 +0100

    Updated dependency checker to warn about Passenger issues

--- lib/barcode_dependency_checker.rb
+++ lib/barcode_dependency_checker.rb
@@ -3,19 +3,23 @@ class BarcodeDependencyChecker
   class GhostScriptMissingException < StandardError;end
   class GhostScriptToPngConversionFailed < StandardError;end
   class BarcodeDirectoryDoesNotExist < StandardError; end
+  PASSENGER_WARNING = "\n If you have passenger installed then you may need to ammend the path environment variable in apache config"
+
   def self.check_image_magick
     convert_filename = `which convert`
     if convert_filename.blank?
-      raise ImageMagickMissingException, "You must have imagemagick installed to generate barcodes"
+      raise ImageMagickMissingException, "You must have imagemagick installed to generate barcodes." + PASSENGER_WARNING
     end
   end
 
   def self.check_ghost_script
     gs_filename = `which gs`
     if gs_filename.blank?
-      raise GhostScriptMissingException, "You must have ghostscript installed to generate barcodes"
+      raise GhostScriptMissingException, "You must have ghostscript installed to generate barcodes" + PASSENGER_WARNING
     end
   end
+
+
 
   def self.check_image_path
     dir = BarcodeGenerator::BARCODE_IMAGES_PATH
